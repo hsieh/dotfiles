b114d19c59ff68ee4b4bccd00623e8d725a8ef73
xieyadong <xieyadong@qianxin.com>
2022-08-01 14:32:14 +0800

    update visrc

--- visrc.lua
+++ visrc.lua
@@ -5,15 +5,26 @@ local plug = require('plugins/vis-plug')
 
 -- configure plugins in an array of tables with git urls and options
 local plugins = {
-	{ url = 'https://repo.or.cz/vis-goto-file.git' },
 	{ url = 'https://gitlab.com/kdsch/vis-go.git' },
-	-- { url = 'https://repo.or.cz/vis-surround.git' },
-	-- { url = 'https://repo.or.cz/vis-toggler.git' },
+	{ url = 'https://repo.or.cz/vis-goto-file.git' },
+	{ url = 'https://repo.or.cz/vis-surround.git' },
+	{ url = 'https://repo.or.cz/vis-toggler.git' },
+	{ url = 'https://repo.or.cz/vis-motsel.git' },
+	{ url = 'https://repo.or.cz/vis-fenced-insert.git' },
+	{ url = 'https://repo.or.cz/vis-toggler.git' },
+	{ url = 'https://repo.or.cz/vis-quickfix.git' },
+	{ url = 'https://repo.or.cz/vis-parkour.git', alias = 'pk' },
+	{ url = 'https://git.sr.ht/~mcepl/vis-fzf-open', alias = 'fzf' },
 	{ url = 'kupospelov/vis-ctags' },
 	{ url = 'erf/vis-highlight', alias = 'hi' },
-	{ url = 'https://git.sr.ht/~mcepl/vis-fzf-open', alias = 'fzf' },
+	{ url = 'erf/vis-cursors' },
 	{ url = 'peaceant/vis-fzf-mru', alias = 'mru', file = 'fzf-mru' },
 	{ url = 'samlwood/vis-gruvbox', theme = true, file = 'gruvbox' },
+	{ url = 'lutobler/vis-modelines' },
+	{ url = 'maciejjan/vis-tmux-repl', file = 'tmux-repl' },
+	-- { url = 'fischerling/vis-lspc'},
+	{ url = 'https://gitlab.com/muhq/vis-lspc', alias = 'lspc'},
+	{ url = 'erf/vis-minimal-theme', theme = true, file = 'dark' },
 }
 
 -- require and optionally install plugins on init
@@ -23,9 +34,8 @@ plug.init(plugins, true)
 plug.plugins.fzf.fzf_path = (
     "FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g \"\"' fzf"
 )
--- Arguments passed to fzf (default: "")
-plug.plugins.fzf.fzf_args = "-q '!.class ' --height=40%"
 
+-- plug.plugins.fzf.fzf_args = "-q '!.class ' --height=40%"
 my_fzf_args = string.gsub([[
     --bind=$my_fzf_key_bindings \
     --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108 \
@@ -58,6 +68,18 @@ my_fzf_args = string.gsub([[
 plug.plugins.fzf.fzf_args = my_fzf_args
 
 plug.plugins.mru.fzfmru_args = "--delimiter / --nth -1"
+
+local gopls = {name = 'gopls', cmd = 'gopls'}
+plug.plugins.lspc.ls_map.go = gopls
+
+-- plug.plugins.lspc.logging = true
+-- plug.plugins.lspc.log_file = '/tmp/vis-lspc-1234'
+
+plug.plugins.pk.auto_square_brackets = true
+plug.plugins.pk.lispwords.scheme.lambda = 0
+plug.plugins.pk.repl_fifo = os.getenv('HOME')..'/.repl_fifo'
+plug.plugins.pk.emacs = false
+plug.plugins.pk.autoselect = true
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
